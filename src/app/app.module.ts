23865dd76fa05d350a9589d466e735844b8ade53
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-08-19 15:39:58 +0200

    delete rama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
   ],
   imports: [
     BrowserModule,
