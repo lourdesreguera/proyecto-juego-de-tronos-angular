21fd63aafec832099e618421fa4d72158b95538a
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-08-22 17:01:17 +0200

    imagen por defecto

--- src/app/shared/component/gallery-characters/gallery-characters.component.ts
+++ src/app/shared/component/gallery-characters/gallery-characters.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GalleryCharactersComponent implements OnInit {
   @Input() characters : any
 
+  imgDefault : any = '../../../../assets/images/default.jpg'
+
   constructor() { }
 
   ngOnInit(): void {
