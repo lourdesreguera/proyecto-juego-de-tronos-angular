9d0ada4dbe59b31e43a059558804645a866d4232
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-08-29 17:00:17 +0200

    languages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
 import { HousesPageComponent } from './pages/houses-page/houses-page.component';
 import { ChronologyComponent } from './pages/chronology/chronology.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './shared/component/footer/footer.component';
 import { GalleryHousesComponent } from './shared/component/gallery-houses/gallery-houses.component';
 import { GalleryCharactersComponent } from './shared/component/gallery-characters/gallery-characters.component';
@@ -16,7 +16,13 @@ import { SimplebarAngularModule } from 'simplebar-angular';
 import { FormsModule } from '@angular/forms';
 import { FilterHousesPipe } from './shared/pipes/filter-houses.pipe';
 import { FilterCharactersPipe } from './shared/pipes/filter-characters.pipe';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { TranslationComponent } from './shared/component/translation/translation.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -32,7 +38,8 @@ import { FilterCharactersPipe } from './shared/pipes/filter-characters.pipe';
     HouseDetailPageComponent,
     FilterHousesPipe,
     HouseDetailPageComponent,
-    FilterCharactersPipe
+    FilterCharactersPipe,
+    TranslationComponent,
 
   ],
   imports: [
@@ -40,7 +47,17 @@ import { FilterCharactersPipe } from './shared/pipes/filter-characters.pipe';
     AppRoutingModule,
     HttpClientModule,
     SimplebarAngularModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http:HttpClient) => {
+          return new TranslateHttpLoader(http)
+        },
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
